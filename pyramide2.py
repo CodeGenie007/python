4d8b2dd49394e07c9360aa467c3b7315159472b3
Gitea <gitea@fake.local>
2018-04-21 15:12:27 -0700

    Removed trailing spaces.

--- pyramide2.py
+++ pyramide2.py
@@ -5,7 +5,7 @@ n = 9
 
 def pyramid(n):       #definening a funcion name and parameters
   i = 1
-  while i <= n:           
+  while i <= n:
     print "line", i,
     j = 1
     while i + j <= n:
@@ -13,11 +13,11 @@ def pyramid(n):       #definening a funcion name and parameters
       j = j + 1
     j = 1
     while j < i:       #prints out first half of the line ,piramide
-      print j,       
+      print j,
       j = j + 1
     j = i               # j already has the right value so this is redundant
     while j >= 1:       #prints out secont half of pyramide
-      print j, 
+      print j,
       j = j - 1
     print
     i = i + 1
@@ -26,4 +26,4 @@ for i in xrange(1,10):
   print
   print 'Pyramid', i
   pyramid(i)
-    
+
